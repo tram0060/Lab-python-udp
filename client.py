711cbbc15cbe7ed98bd4b6d2df5c4a45f25c5990
Mamadou Traore <tram0060@uqar.ca>
2026-02-15 22:23:58 -0500

    Parti4 Introducrion a la hashlib

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-
+#PARTIE2
 from pathlib import Path
 p = Path("data/message.txt") 
 s = p.read_text(encoding="utf-8") 
@@ -13,7 +13,8 @@ s2 = b.decode("utf-8")
 print(len(b2), type(b2)) 
 print(type(s2))  
 
-
+#PARTIE3 
+"""
 import socket
 
 HOST = "127.0.0.1"
@@ -29,4 +30,27 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         print(f"Reçu {len(data)} octets de {addr}")
         print("Message :", data.decode("utf-8", errors="replace"))
 
-        s.sendto(b"Message recu", addr)
+        s.sendto(b"Message recu", addr)*/
+
+"""
+        #PARTIE4
+import socket
+import hashlib
+from pathlib import Path
+
+HOST = "127.0.0.1"
+PORT = 12346
+
+#Ici c'est pour lire message
+msg = Path("data/message.txt").read_text(encoding="utf-8").encode("utf-8")
+
+h = hashlib.sha256(msg).hexdigest()
+
+# Créer payload : message + séparateur + hash
+payload = msg + b"\x00" + h.encode("ascii")
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(payload, (HOST, PORT))
+
+    data, _ = s.recvfrom(1024)
+    print("Réponse serveur :", data.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#PARIE3
 import socket
 from pathlib import Path
 
@@ -11,3 +12,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
     data, _ = s.recvfrom(1024)
     print("Réponse du serveur :", data.decode("utf-8"))
+#PARTIE4
+import socket
+import hashlib
+
+HOST = "127.0.0.1"
+PORT = 12346
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST, PORT))
+    print(f"Serveur UDP sur {HOST}:{PORT}")
+
+    while True:
+        data, addr = s.recvfrom(2048)
+
+        try:
+            # Séparer message et hash
+            message, hash_hex = data.split(b"\x00", 1)
+
+            # Recalculer hash
+            calc = hashlib.sha256(message).hexdigest().encode("ascii")
+
+            if calc == hash_hex:
+                print("Message valide reçu")
+                s.sendto(b"Message et hachage valides", addr)
+            else:
+                print("Hash invalide")
+                s.sendto(b"Erreur de hachage", addr)
+
+        except Exception:
+            s.sendto(b"Format invalide", addr)
