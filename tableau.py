0c136de7d06b7d8dd62d920db0990c39182f4980
Mamadou Traore <tram0060@uqar.ca>
2026-02-17 16:53:16 -0500

    Partie 5 inverser le bit

--- tableau.py
+++ tableau.py
@@ -11,9 +11,20 @@ print(chr(data[0]))
 data = bytearray(b"ABC")  
 print(bytes(data))    
 data[0] = 66 
-"""
+
+
 #slicing (tranches) 
 data = b"ABCDEFG" 
 print(data[0:3])   
 print(data[3:])  
+
+"""
+#XOR pour inverser un bit 
+x = 0b01010101 
+y = x ^ 0b00000001 
+print(bin(x), bin(y))
+
+ba = bytearray(b"A")    
+ba[0] ==  1 
+print(bytes(ba))  
 
