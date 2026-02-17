81d091bfa1db8bc5c02bb118619cf61e74ae4ddb
Mamadou Traore <tram0060@uqar.ca>
2026-02-17 16:37:06 -0500

    Parti 5 slicing (tranches)

--- tableau.py
+++ tableau.py
@@ -4,10 +4,16 @@ data = b"ABC"
 print(data)           
 print(data[0])   
 print(chr(data[0]))
-"""
+
 
 #Bytesarray
 
 data = bytearray(b"ABC")  
 print(bytes(data))    
-data[0] = 66   
+data[0] = 66 
+"""
+#slicing (tranches) 
+data = b"ABCDEFG" 
+print(data[0:3])   
+print(data[3:])  
+
