b757e67af4eb3cef470fd9ede7e3e6ad26824b11
Mamadou Traore <tram0060@uqar.ca>
2026-02-17 16:33:19 -0500

    Parti 5 Bytearray ou tableau modifiable

--- tableau.py
+++ tableau.py
@@ -1,4 +1,13 @@
+"""#BYTES
+
 data = b"ABC" 
 print(data)           
 print(data[0])   
 print(chr(data[0]))
+"""
+
+#Bytesarray
+
+data = bytearray(b"ABC")  
+print(bytes(data))    
+data[0] = 66   
